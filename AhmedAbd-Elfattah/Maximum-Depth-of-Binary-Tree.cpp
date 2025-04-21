838bdba0f9ae4b95f671ed2195b11a45ec28621d
AhmedAbd-ElFattahCM <ahmed.abdelfattah@cairomotive.com>
2025-04-21 07:11:39 +0200

    [104] [Time Beats: 100.00%] [Memory Beats: 13.20%] - LeetPush

---
+++ AhmedAbd-Elfattah/Maximum-Depth-of-Binary-Tree.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root, size_t level=0) {
+    if (root == nullptr) {
+        return level;
+    }
+    return std::max(maxDepth(root->right, level + 1), maxDepth(root->left, level + 1));
+}
+};
