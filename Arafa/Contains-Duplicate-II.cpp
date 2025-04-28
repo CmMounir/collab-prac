ae8f7dc465fc03141d3d67a9033a3a0373983eb7
AhmmedArafa <ahmad.arafa@cairomotive.com>
2025-04-28 13:55:00 +0300

    [219] [Time Beats: 5.01%] [Memory Beats: 93.72%] - LeetPush

---
+++ Arafa/Contains-Duplicate-II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j-i <= k; j++) {
+                if(j <n && nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+};
