3e910b338f31acce43a6b83d6f8a7be630d619d0
AhmedAbd-ElFattahCM <ahmed.abdelfattah@cairomotive.com>
2025-04-28 13:40:42 +0300

    [219] [Time Beats: 19.37%] [Memory Beats: 5.02%] - LeetPush

--- AhmedAbd-Elfattah/Contains-Duplicate-II.cpp
+++ AhmedAbd-Elfattah/Contains-Duplicate-II.cpp
@@ -3,19 +3,19 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
     int window_size = std::min(k + 1, static_cast<int>(nums.size()));
 
-    std::set<int> seen_in_window;
+    std::set<int> window;
     for (int i = 0; i < window_size; i++) {
-        seen_in_window.insert(nums.at(i));
+        window.insert(nums.at(i));
     }
 
-    if (seen_in_window.size() != window_size) {
+    if (window.size() != window_size) {
         return true;
     }
 
     for (int i = 0; i < (nums.size()- window_size); i++) {
-        seen_in_window.erase(nums.at(i));
-        seen_in_window.insert(nums.at(i+ window_size));
-        if (seen_in_window.size() != window_size) {
+        window.erase(nums.at(i));
+        window.insert(nums.at(i+ window_size));
+        if (window.size() != window_size) {
             return true;
         }
     }
