f17d0729e4528e4afb377a8240ccd43bd5e374d9
AhmedAbd-ElFattahCM <ahmed.abdelfattah@cairomotive.com>
2025-04-28 13:38:42 +0300

    [205] [Time Beats: 57.81%] [Memory Beats: 55.82%] - LeetPush

--- AhmedAbd-Elfattah/Isomorphic-Strings.cpp
+++ AhmedAbd-Elfattah/Isomorphic-Strings.cpp
@@ -5,18 +5,23 @@ public:
     std::unordered_map<char, char> map_t_to_s;
 
     for (int i = 0; i < t.size(); i++) {
-        if (map_s_to_t.find(s.at(i)) == map_s_to_t.end()) {
+        if (map_s_to_t.find(s.at(i))==map_s_to_t.end()) {
+
             map_s_to_t.insert(std::make_pair(s.at(i), t.at(i)));
         }
+
         if (map_s_to_t.at(s.at(i)) != t.at(i)) {
             return false;
         }
+
         if (map_t_to_s.find(t.at(i)) == map_t_to_s.end()) {
             map_t_to_s.insert(std::make_pair(t.at(i), s.at(i)));
         }
+        
         if (map_t_to_s.at(t.at(i)) != s.at(i)) {
             return false;
         }
+
     }
 
     return true;
