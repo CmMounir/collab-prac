7e7ca58c464f7f4ec3a97ccc35b12f30dcd23bba
AhmmedArafa <ahmad.arafa@cairomotive.com>
2025-05-02 12:20:33 +0300

    [141] [Time Beats: 5.83%] [Memory Beats: 8.40%] - LeetPush

---
+++ Arafa/Linked-List-Cycle.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        map<ListNode*, int> myMap;
+        while(head != nullptr) {
+            
+            if(myMap[head] == 1) {
+                return true;
+            }
+            else {
+                myMap[head] = 1;
+            }
+            head = head->next;
+        }
+        return false;
+    }
+};
