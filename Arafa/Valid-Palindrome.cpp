176ad9e62e479ac9770d03fee9cbbf9099a95a69
AhmmedArafa <ahmad.arafa@cairomotive.com>
2025-05-22 17:43:59 +0300

    [125] [Time Beats: 100.00%] [Memory Beats: 42.83%] - LeetPush

---
+++ Arafa/Valid-Palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size()-1;
+        while(left < right) {
+            if(!iswalnum(s[left])) {left++; continue;}
+            if(!iswalnum(s[right])) {right--; continue;}
+
+            if(tolower(s[left]) != tolower(s[right])) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
