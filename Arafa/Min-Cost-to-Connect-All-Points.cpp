793590feeb7de0d2101c82aea8782f21d6cd6729
AhmmedArafa <ahmad.arafa@cairomotive.com>
2025-06-17 22:56:06 +0300

    [1584] [Time Beats: 30.01%] [Memory Beats: 15.05%] - LeetPush

--- Arafa/Min-Cost-to-Connect-All-Points.cpp
+++ Arafa/Min-Cost-to-Connect-All-Points.cpp
@@ -1,42 +1,56 @@
 class Solution {
 public:
-    int primMST(vector<vector<pair<int, int>>>& graph, int V) {
-        vector<bool> inMST(V, false);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // (weight, vertex)
-
-        pq.push({0, 0}); // Start from node 0
-        int mst_weight = 0;
-
-        while (!pq.empty()) {
-            auto [weight, u] = pq.top(); pq.pop();
-
-            if (inMST[u]) continue;
-            inMST[u] = true;
-            mst_weight += weight;
-
-            for (auto [v, w] : graph[u]) {
-                if (!inMST[v])
-                    pq.push({w, v});
-            }
+    struct Compare {
+        bool operator()(const vector<int>& a, const vector<int>& b) const {
+            return a[2] > b[2];  // Min-heap based on distance
         }
+    };
 
-        return mst_weight;
+    priority_queue<vector<int>, vector<vector<int>>, Compare> pq;
+
+    int calcDistance(const vector<int>& p1, const vector<int>& p2) {
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
     }
 
-    int calcDistance(vector<int> p1 , vector<int> p2) {
-        return abs(p1[0]-p2[0])+abs(p1[1]-p2[1]);
+    int findParent(vector<int>& parent, int node) {
+        if (parent[node] != node)
+            parent[node] = findParent(parent, parent[node]); // Path compression
+        return parent[node];
+    }
+
+    bool unionNodes(vector<int>& parent, int u, int v) {
+        int pu = findParent(parent, u);
+        int pv = findParent(parent, v);
+        if (pu == pv) return false; // They are already connected (cycle)
+
+        parent[pu] = pv;
+
+        return true;
+    }
+
+    int kruskalMST(int n) {
+        vector<int> parent(n);
+        for (int i = 0; i < n; ++i) parent[i] = i;
+
+        int result = 0, edgesUsed = 0;
+        while (!pq.empty() && edgesUsed < n - 1) {
+            auto edge = pq.top(); pq.pop();
+            int u = edge[0], v = edge[1], weight = edge[2];
+            if (unionNodes(parent, u, v)) {
+                result += weight;
+                edgesUsed++;
+            }
+        }
+        return result;
     }
 
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<vector<pair<int, int>>> graph(points.size());
-
-        for(int i = 0; i < points.size(); i++) {
-            for(int j = 0; j < points.size(); j++) {
-                if(i == j) continue;
-                graph[i].push_back({j, calcDistance(points[i], points[j])});
+        int n = points.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                pq.push({i, j, calcDistance(points[i], points[j])});
             }
-        } 
-
-        return primMST(graph, points.size());
-    }   
+        }
+        return kruskalMST(n);
+    }
 };
