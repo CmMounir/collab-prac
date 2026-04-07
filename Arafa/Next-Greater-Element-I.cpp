57ba46326b59c76ac49615f4bf9783af1b580add
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-07 12:10:10 +0200

    [496] [Time Beats: 41.03%] [Memory Beats: 59.05%] - LeetPush

--- Arafa/Next-Greater-Element-I.cpp
+++ Arafa/Next-Greater-Element-I.cpp
@@ -1,30 +1,31 @@
-class Solution {
-public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result;
-        for(const auto& element : nums1) {
-            for(int i = 0; i < nums2.size(); i++) {
-                if(element == nums2[i]) {
-                    if(i+1 == nums2.size()) {
-                        result.push_back(-1);
-                    }
-                    else {
-                        bool flag = false;
-                        int t;
-                        for(t = i+1; t < nums2.size(); t++) {
-                            if(nums2[t] > element) { 
-                                flag = true; 
-                                break;
-                            }
-                        }
-                        if(flag)
-                            result.push_back(nums2[t]);
-                        else 
-                            result.push_back(-1);
-                    }
-                }
-            }
-        }
-        return result;
-    }
-};
+1class Solution {
+2public:
+3    int getNextAfterIndex(int index, vector<int>& nums)
+4    {
+5        int res = -1;
+6        for(int i =index+1; i < nums.size(); i++)
+7        {
+8            if(nums[i] > nums[index]) {
+9                res = nums[i];
+10                break;
+11            }
+12        }
+13        return res;
+14    }
+15    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+16        vector<int> res(nums1.size());
+17        map<int, int> myMap;
+18
+19        for(int i = 0; i < nums2.size(); i++)
+20        {
+21            myMap[nums2[i]] = getNextAfterIndex(i, nums2);
+22        }
+23
+24        for(int i = 0; i < nums1.size(); i++)
+25        {
+26            res[i] = myMap[nums1[i]];
+27        }
+28
+29        return res;
+30    }
+31};
