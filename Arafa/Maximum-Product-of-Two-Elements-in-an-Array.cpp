0d1ef6ae73f5ffe669907564d515da7e0c90451b
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-07 12:53:19 +0200

    [1464] [Time Beats: 100.00%] [Memory Beats: 12.11%] - LeetPush

--- Arafa/Maximum-Product-of-Two-Elements-in-an-Array.cpp
+++ Arafa/Maximum-Product-of-Two-Elements-in-an-Array.cpp
@@ -1,9 +1,14 @@
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        priority_queue<int> myHeap(nums.begin(), nums.end());
-        int x = myHeap.top(); myHeap.pop();
-        int y = myHeap.top(); myHeap.pop();
-        return (x-1)*(y-1);
-    }
-};
+1class Solution {
+2public:
+3    int maxProduct(vector<int>& nums) {
+4        priority_queue<int> q;
+5        for(const auto& element : nums)
+6        {
+7            q.push(element);
+8        }
+9        int n1 = q.top(); q.pop();
+10        int n2 = q.top(); q.pop();
+11
+12        return (n1-1)*(n2-1);
+13    }
+14};
