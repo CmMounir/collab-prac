728d546e251311a63ba4a7e930dfb2dba0fdfa64
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-07 12:12:39 +0200

    [1] [Time Beats: 72.17%] [Memory Beats: 18.34%] - LeetPush

--- Arafa/Two-Sum.cpp
+++ Arafa/Two-Sum.cpp
@@ -1,16 +1,30 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        for(int i = 0; i < nums.size(); i++) {
-            for(int t = i+1; t < nums.size(); t++) {
-                if(nums[i]+nums[t] == target) {
-                    result.push_back(i);
-                    result.push_back(t);
-                    return result;
-                }
-            }
-        }
-        return result;
-    }
-};
+1class Solution {
+2public:
+3    // vector<int> twoSum(vector<int>& nums, int target) {
+4    //     vector<int> res;
+5    //     for(int i = 0; i < nums.size()-1; i++)
+6    //     {
+7    //         for(int j = i+1; j < nums.size(); j++)
+8    //         {
+9    //             if(nums[i] + nums[j] == target)
+10    //             {
+11    //                 return {i,j};
+12    //             }
+13    //         }
+14    //     }
+15    //     return res;
+16    // }
+17
+18    vector<int> twoSum(vector<int> &nums, int target) {
+19        unordered_map<int, int> hash;
+20        for (int i = 0; i < nums.size(); ++i) {
+21            int complement = target - nums[i];
+22            if (hash.find(complement) != hash.end()) {
+23                return {hash[complement], i};
+24            }
+25            hash[nums[i]] = i;
+26        }
+27        // Return an empty vector if no solution is found
+28        return {};
+29    }
+30};
