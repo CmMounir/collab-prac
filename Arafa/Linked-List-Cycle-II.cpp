043a0b1f8d99a39881d281b5568776bf639412d9
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-09 10:19:43 +0200

    [142] [Time Beats: 15.05%] [Memory Beats: 8.11%] - LeetPush

--- Arafa/Linked-List-Cycle-II.cpp
+++ Arafa/Linked-List-Cycle-II.cpp
@@ -1,22 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
-        map<ListNode *, int> myMap;
-        while(head != nullptr) {
-            if(myMap[head] == 1)
-                return head;
-            else 
-                myMap[head] = 1;
-            head = head->next;
-        }
-        return nullptr;
-    }
-};
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode(int x) : val(x), next(NULL) {}
+7 * };
+8 */
+9class Solution {
+10public:
+11    ListNode *detectCycle(ListNode *head) {
+12        ListNode* temp = head;
+13        ListNode* res = nullptr;
+14        map<ListNode*, bool> myMap;
+15        while(temp != nullptr)
+16        {
+17            if(myMap[temp] == false) {
+18                myMap[temp] = true;
+19                res = temp->next;
+20            }
+21            else {
+22                break; 
+23            }
+24            temp = temp->next;
+25        }
+26        return res;
+27    }
+28};
