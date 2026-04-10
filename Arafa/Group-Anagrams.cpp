51c0ea0d9104d6a321afef5b9f777406db6e05a1
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-10 13:35:48 +0200

    [49] [Time Beats: 37.96%] [Memory Beats: 67.01%] - LeetPush

--- Arafa/Group-Anagrams.cpp
+++ Arafa/Group-Anagrams.cpp
@@ -1,21 +1,19 @@
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    map<string, vector<string>> myMap;
-
-    for(int i = 0; i < strs.size(); i++) {
-       string temp = strs[i];
-       std::sort(temp.begin(), temp.end()); 
-       auto x =myMap[temp];
-       x.push_back(strs[i]);
-       myMap[temp] = x;
-    }
-
-    vector<vector<string>> result;
-    for(const auto& element : myMap) {
-       result.push_back(element.second); 
-    }
-
-    return result;
-    }
-};
+1class Solution {
+2public:
+3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+4        vector<vector<string>> res;
+5
+6        map<string, vector<string>> myMap;
+7        for(auto& str : strs) {
+8            string temp = str;
+9            sort(str.begin(), str.end());
+10            myMap[str].push_back(temp);
+11        }
+12
+13        for(const auto& pair : myMap) {
+14            res.push_back(pair.second);
+15        }
+16
+17        return res;
+18    }
+19};
