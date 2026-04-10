00be69b29aab347f46e89cffacd3b2c9ac2d6250
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-10 13:42:02 +0200

    [387] [Time Beats: 10.29%] [Memory Beats: 7.82%] - LeetPush

--- Arafa/First-Unique-Character-in-a-String.cpp
+++ Arafa/First-Unique-Character-in-a-String.cpp
@@ -1,17 +1,27 @@
-class Solution {
-public:
-    int firstUniqChar(string s) {
-        std::unordered_map<char, int> myMap;
-        
-        for(char x : s) {
-            myMap[x]++;
-        }
-
-    for(int i =0 ; i < s.length(); i++) {
-        if(myMap[s[i]] == 1) {
-            return i;
-        }
-    }
-    return -1;
-    }
-};
+1class Solution {
+2public:
+3    int firstUniqChar(string s) {
+4        int res = -1;
+5        map<char, int> myMap;
+6        map<char, bool> mySecondMap;
+7
+8        for(const auto& c : s) {
+9            myMap[c]++;
+10        }
+11
+12        for(const auto& pair : myMap) {
+13            if(pair.second == 1) {
+14                mySecondMap[pair.first] = true;
+15            }
+16        }
+17
+18        for(int i = 0; i < s.length(); i++) {
+19            if(mySecondMap[s[i]]) {
+20                res = i;
+21                break;
+22            }
+23        }
+24
+25        return res;
+26    }
+27};
