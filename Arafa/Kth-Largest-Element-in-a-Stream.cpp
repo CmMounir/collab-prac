df12736a8c1908ed75c05f5fba0785d08944d643
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-11 17:33:56 +0200

    [703] [Time Beats: 100.00%] [Memory Beats: 24.89%] - LeetPush

--- Arafa/Kth-Largest-Element-in-a-Stream.cpp
+++ Arafa/Kth-Largest-Element-in-a-Stream.cpp
@@ -1,27 +1,32 @@
-class KthLargest {
-private:
-    int k;
-    priority_queue<int, vector<int>, greater<int>> pq;
-public:
-    KthLargest(int k, vector<int>& nums) {
-        this->k = k;
-        auto endPointer = nums.begin() + min(k, (int)nums.size());
-        pq = priority_queue<int, vector<int>, greater<int>> (nums.begin(), endPointer);
-        for(int i = k; i < nums.size(); ++i) {
-            pq.push(nums[i]);
-            pq.pop();
-        }
-    }
-    
-    int add(int val) {
-        pq.push(val);
-        if(pq.size() > k) pq.pop();
-        return pq.top();
-    }
-};
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest* obj = new KthLargest(k, nums);
- * int param_1 = obj->add(val);
- */
+1class KthLargest {
+2public:
+3    priority_queue<int, vector<int>, greater<int>> pq;
+4    int mK;
+5
+6    KthLargest(int k, vector<int>& nums) {
+7        mK = k;
+8        
+9        for (const auto& element : nums) {
+10            pq.push(element);
+11            if (pq.size() > mK) {
+12                pq.pop(); // keep only k largest
+13            }
+14        }
+15    }
+16    
+17    int add(int val) {
+18        pq.push(val);
+19        
+20        if (pq.size() > mK) {
+21            pq.pop(); // remove smallest
+22        }
+23
+24        return pq.top(); // kth largest
+25    }
+26};
+27
+28/**
+29 * Your KthLargest object will be instantiated and called as such:
+30 * KthLargest* obj = new KthLargest(k, nums);
+31 * int param_1 = obj->add(val);
+32 */
