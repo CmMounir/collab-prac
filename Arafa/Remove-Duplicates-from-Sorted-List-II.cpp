26b825d2537d6e47414302abff9df1d6c0157693
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-11 14:13:41 +0200

    [82] [Time Beats: 100.00%] [Memory Beats: 6.07%] - LeetPush

--- Arafa/Remove-Duplicates-from-Sorted-List-II.cpp
+++ Arafa/Remove-Duplicates-from-Sorted-List-II.cpp
@@ -1,41 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        map<int, int> myMap;
-        ListNode* temp = head;
-        while(temp) {
-            myMap[temp->val]++;
-            temp = temp->next;
-        }
-
-        ListNode* new_head = nullptr;
-        ListNode* tail = nullptr;
-        ListNode* helper = nullptr;
-        
-        for(const auto& pair : myMap) {
-            if(pair.second == 1) {
-                if(new_head == nullptr) {
-                helper = new ListNode(pair.first);
-                new_head = helper;
-                tail = helper;
-                }        
-                else {
-                helper = new ListNode(pair.first);
-                tail->next = helper;
-                tail = helper;
-                }
-            }
-        }
-        return new_head;
-    }
-};
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* deleteDuplicates(ListNode* head) {
+14        ListNode* new_head = nullptr;
+15        ListNode* helper;
+16
+17        map<int, int> myMap;
+18
+19        while(head != nullptr) {
+20            myMap[head->val]++;
+21            head = head->next;
+22        }
+23
+24        for(const auto& pair : myMap) {
+25            if(pair.second == 1) {
+26                if(new_head == nullptr) {
+27                    new_head = new ListNode(pair.first);
+28                    helper = new_head;
+29                }
+30                else {
+31                    ListNode* node = new ListNode(pair.first);
+32                    helper->next = node;
+33                    helper = node;
+34                }
+35            }
+36        }
+37
+38        return new_head;
+39    }
+40};
