dd4372a30e6cf7773139038a0c726bdd7ecc4c32
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-12 10:55:43 +0200

    [111] [Time Beats: 58.73%] [Memory Beats: 90.59%] - LeetPush

--- Arafa/Minimum-Depth-of-Binary-Tree.cpp
+++ Arafa/Minimum-Depth-of-Binary-Tree.cpp
@@ -1,31 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    void solve(TreeNode* root, int index, int& min) {
-        if(root == nullptr) return ;
-    
-        if(root->left == nullptr && root->right == nullptr) {
-            if(min > index) 
-                min = index;
-            return ;
-        }
-        
-        solve(root->left, index+1, min);
-        solve(root->right, index+1, min);
-    }
-    int minDepth(TreeNode* root) {
-        int min = INT_MAX;
-        solve(root, 1, min);
-        return (min==INT_MAX ? 0 : min);
-    }
-};
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    int ans = INT_MAX;
+15
+16    void traverse(TreeNode* root, int counter) {
+17        if(root == nullptr) {
+18            return;
+19        }
+20
+21        if(root != nullptr && root->left == nullptr && root->right == nullptr) {
+22            if(ans > counter) {
+23                ans = counter;
+24            }
+25            return;
+26        }
+27
+28        traverse(root->left, counter+1);
+29        traverse(root->right, counter+1);
+30    }
+31
+32    int minDepth(TreeNode* root) {
+33        traverse(root, 1);
+34        return ans==INT_MAX?0:ans;
+35    }
+36};
