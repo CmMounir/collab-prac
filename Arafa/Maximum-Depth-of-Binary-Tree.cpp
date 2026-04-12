7ba4fadb244bbcbc2531584e4d1e6cc60a43df93
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-12 10:44:10 +0200

    [104] [Time Beats: 100.00%] [Memory Beats: 13.69%] - LeetPush

--- Arafa/Maximum-Depth-of-Binary-Tree.cpp
+++ Arafa/Maximum-Depth-of-Binary-Tree.cpp
@@ -1,30 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    void solve(TreeNode* root, int index, int &max) {
-        if(root == nullptr) return;
-
-        if(root->left == nullptr && root->right == nullptr) {
-            if(max < index) max = index; 
-            return;
-        }
-
-        solve(root->left, index+1, max);
-        solve(root->right, index+1, max);
-    }
-    int maxDepth(TreeNode* root) {
-        int max = 0;
-        solve(root, 1, max);
-        return max;
-    }
-};
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    int ans = 0;
+15    void traverse(TreeNode* root, int counter) {
+16        if(root == nullptr) {
+17            if(ans < counter)
+18                ans = counter;
+19            return;
+20        }
+21
+22        traverse(root->left, counter+1);
+23
+24        traverse(root->right, counter+1);
+25    }
+26
+27    int maxDepth(TreeNode* root) {
+28        traverse(root,0);
+29        return ans;
+30    }
+31};
