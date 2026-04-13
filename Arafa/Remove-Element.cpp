733bc18996abf44911157025a599798b205a48b4
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-13 15:25:25 +0200

    [27] [Time Beats: 100.00%] [Memory Beats: 47.83%] - LeetPush

--- Arafa/Remove-Element.cpp
+++ Arafa/Remove-Element.cpp
@@ -1,19 +1,18 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        int k = 0;
-        vector<int> myVector;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != val) {
-                k++;
-                myVector.push_back(nums[i]);
-            }
-        }
-
-        for(int i = 0; i < myVector.size(); i++) {
-            nums[i] = myVector[i];
-        }
-
-        return k;
-    }
-};
+1class Solution {
+2public:
+3    int removeElement(vector<int>& nums, int val) {
+4        vector<int> vec;
+5        for(auto& element : nums) {
+6            if(element != val)
+7                vec.push_back(element);
+8        }
+9        
+10        int counter = 0;
+11        for(auto& element : vec) {
+12            nums[counter++] = element;
+13        }
+14
+15
+16        return counter;
+17    }
+18};
