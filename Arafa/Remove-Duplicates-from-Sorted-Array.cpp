6a92dd17634f60b987d080040434b2bb5fc42b5e
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-13 15:11:44 +0200

    [26] [Time Beats: 100.00%] [Memory Beats: 18.27%] - LeetPush

--- Arafa/Remove-Duplicates-from-Sorted-Array.cpp
+++ Arafa/Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,36 +1,26 @@
 1class Solution {
 2public:
 3    int removeDuplicates(vector<int>& nums) {
-4        if(nums.size() == 0)
-5            return 0;
+4        int previous;
+5        int lastGoodPosition = 0;
 6
-7        int res = 0;
-8
-9        
-10        int previous;
-11        int lastGoodPosition = 0;
-12        /*
-13            1 2 2 3 
+7        for(int i = 0; i < nums.size(); i++) {
+8            if(i == 0) {
+9                lastGoodPosition = 0;
+10            }
+11            else {
+12                if(nums[i] == previous) // duplicate
+13                {
 14
-15            1 1 2
-16        */
-17        for(int i = 0; i < nums.size(); i++) {
-18            if(i == 0) {
-19                lastGoodPosition = 0;
+15                }
+16                else {
+17                    nums[lastGoodPosition+1] = nums[i];
+18                    lastGoodPosition++;
+19                }
 20            }
-21            else {
-22                if(nums[i] == previous) // duplicate
-23                {
-24
-25                }
-26                else {
-27                    nums[lastGoodPosition+1] = nums[i];
-28                    lastGoodPosition++;
-29                }
-30            }
-31            previous = nums[i];
-32        }
-33
-34        return lastGoodPosition+1;
-35    }
-36};
+21            previous = nums[i];
+22        }
+23
+24        return lastGoodPosition+1;
+25    }
+26};
