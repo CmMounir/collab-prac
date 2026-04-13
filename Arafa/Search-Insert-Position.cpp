2acbb68c9d50f590a5949f5fdb8d2618f83af341
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-13 11:28:10 +0200

    [35] [Time Beats: 100.00%] [Memory Beats: 41.93%] - LeetPush

--- Arafa/Search-Insert-Position.cpp
+++ Arafa/Search-Insert-Position.cpp
@@ -1,13 +1,27 @@
-class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        int result;
-        for(int i =0; i < nums.size(); i++) {
-            if(nums[i] >= target)  {
-                result = i;
-                break;
-            }
-        }
-        return result;
-    }
-};
+1class Solution {
+2public:
+3    int searchInsert(vector<int>& nums, int target) {
+4        int left = 0;
+5        int right = nums.size()-1;
+6        int mid = 0;
+7
+8        while(left <= right) {
+9            mid = (left+right)/2;
+10
+11            if(nums[mid] == target) {
+12                return mid;
+13            }
+14            else if(nums[mid] > target) {
+15                right = mid-1;
+16            }
+17            else {
+18                left = mid+1;
+19            }
+20        }   
+21
+22        if(nums[mid] < target) 
+23            return mid+1;
+24        else
+25            return mid;
+26    }
+27};
