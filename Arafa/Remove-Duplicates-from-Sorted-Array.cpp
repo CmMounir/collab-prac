5434e2977c0a47ed3f19a991b8d2eb194b2c2b9b
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-13 15:10:53 +0200

    [26] [Time Beats: 100.00%] [Memory Beats: 18.27%] - LeetPush

--- Arafa/Remove-Duplicates-from-Sorted-Array.cpp
+++ Arafa/Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,18 +1,36 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        set<int> mySet;
-
-        for(int i= 0; i < nums.size(); i++) {
-           mySet.insert(nums[i]); 
-        }
-
-        int i = 0;
-        for (const int& element : mySet) {
-            nums[i] = element;
-            i++;
-        }
-
-        return mySet.size();
-    }
-};
+1class Solution {
+2public:
+3    int removeDuplicates(vector<int>& nums) {
+4        if(nums.size() == 0)
+5            return 0;
+6
+7        int res = 0;
+8
+9        
+10        int previous;
+11        int lastGoodPosition = 0;
+12        /*
+13            1 2 2 3 
+14
+15            1 1 2
+16        */
+17        for(int i = 0; i < nums.size(); i++) {
+18            if(i == 0) {
+19                lastGoodPosition = 0;
+20            }
+21            else {
+22                if(nums[i] == previous) // duplicate
+23                {
+24
+25                }
+26                else {
+27                    nums[lastGoodPosition+1] = nums[i];
+28                    lastGoodPosition++;
+29                }
+30            }
+31            previous = nums[i];
+32        }
+33
+34        return lastGoodPosition+1;
+35    }
+36};
