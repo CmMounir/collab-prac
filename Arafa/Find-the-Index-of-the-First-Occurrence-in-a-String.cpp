c5d021664e216011d4d394b6bc6396751f0ebc6c
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-14 11:06:32 +0200

    [28] [Time Beats: 24.95%] [Memory Beats: 46.16%] - LeetPush

--- Arafa/Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
+++ Arafa/Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
@@ -1,16 +1,26 @@
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        for(int i = 0; i < haystack.length(); i++) {
-            int t;
-            for(t = 0; t < needle.length(); t++) {
-                if(haystack[i+t] != needle[t]) 
-                    break;
-            }
-            if(t == needle.length()) {
-                return i;
-            }
-        }
-        return -1;
-    }
-};
+1class Solution {
+2public:
+3    int strStr(string haystack, string needle) {
+4        if(haystack.length() < needle.length())
+5            return -1;
+6        
+7        if(haystack.length() == needle.length() && haystack == needle)
+8            return 0;
+9
+10        int res = -1;
+11        for(int i = 0; i <= haystack.length()-needle.length(); i++) {
+12            bool flag = true;
+13            for(int j =0; j < needle.length(); j++) {
+14                if(haystack[i+j] != needle[j]) {
+15                    flag = false;
+16                    break;
+17                }
+18            }
+19            if(flag == true) {
+20                res = i;
+21                break;
+22            }
+23        }
+24        return res;
+25    }
+26};
