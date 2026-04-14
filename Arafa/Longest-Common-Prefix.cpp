1422368467774ac1fed77cf8da89aa02a2aef508
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-14 10:49:34 +0200

    [14] [Time Beats: 100.00%] [Memory Beats: 94.23%] - LeetPush

--- Arafa/Longest-Common-Prefix.cpp
+++ Arafa/Longest-Common-Prefix.cpp
@@ -1,25 +1,31 @@
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-       vector<int> sizes;
-       for(int index= 0; index < strs.size(); index++) {
-            sizes.push_back(strs[index].length());
-       }
-
-       int i = 0;
-       string result = "";
-       int min_str_size = *min_element(sizes.begin(), sizes.end());
-       while(i < min_str_size) {
-        char letter = strs[0][i];
-        for(int index= 1; index < strs.size(); index++) {
-            if(strs[index][i] != letter) {
-                return result;
-            }
-        }
-        result += strs[0][i];
-        i++;
-       }
-
-       return result;
-    }
-};
+1class Solution {
+2public:
+3    string longestCommonPrefix(vector<string>& strs) {
+4        string res = "";
+5
+6        int min = INT_MAX;
+7        for(const auto& str : strs) {
+8            if(min > str.length()) {
+9                min = str.length();
+10            }
+11        }
+12
+13        for(int i = 0; i < min; i++) {
+14            bool flag = true;
+15            for(auto& str : strs) {
+16                if(strs[0][i] != str[i]) {
+17                    flag = false;
+18                    break;
+19                }
+20            }
+21            if(flag == true) {
+22                res += strs[0][i];
+23            }
+24            else {
+25                break;
+26            }
+27        }
+28
+29        return res;
+30    }
+31};
