57358fccd11bddddbd2e5392f46f3bf3ea49b16e
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-14 10:20:24 +0200

    [557] [Time Beats: 22.93%] [Memory Beats: 19.40%] - LeetPush

--- Arafa/Reverse-Words-in-a-String-III.cpp
+++ Arafa/Reverse-Words-in-a-String-III.cpp
@@ -2,25 +2,16 @@
 2public:
 3    string reverseWords(string s) {
 4        string res = "";
-5
-6        vector<string> myVec;
-7        string temp = "";
-8        for(int i = 0; i < s.length(); i++) {
-9            if(s[i] == ' ') {
-10                myVec.push_back(temp);
-11                temp = "";
-12            }
-13            else {
-14                temp += s[i];
-15            }
-16        }
-17        myVec.push_back(temp);
-18        
-19        for(auto& element : myVec) {
-20            reverse(element.begin(), element.end());
-21            res += element + " ";
-22        }
-23
-24        return string(res.begin(), res.end()-1);
-25    }
-26};
+5        stringstream ss(s);
+6        
+7        string word;
+8        while(ss >> word) {
+9            reverse(word.begin(), word.end());    
+10            res += word + " ";
+11        }
+12        
+13        res.pop_back();
+14        
+15        return res;
+16    }
+17};
