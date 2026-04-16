c54acfd5be58d233f200fa9ecbe54ea05025ec6f
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-16 10:58:04 +0200

    [58] [Time Beats: 100.00%] [Memory Beats: 33.01%] - LeetPush

--- Arafa/Length-of-Last-Word.cpp
+++ Arafa/Length-of-Last-Word.cpp
@@ -1,19 +1,18 @@
-class Solution {
-public:
-    int lengthOfLastWord(string s) {
-        int result = 0;
-        bool first = true;
-        for(int i = s.length()-1; i >= 0; i--) {
-            if(s[i] == ' ') { 
-                if(result == 0) 
-                    continue;
-                else
-                    break;
-            }
-            else {
-                result++;
-            }
-        }
-        return result;
-    }
-};
+1class Solution {
+2public:
+3    int lengthOfLastWord(string s) {
+4        int res = 0;
+5        bool flag = false;
+6
+7        for(int i = s.length()-1; i > -1; i--) {
+8            if(s[i] == ' ') {
+9               if(res > 0) break;
+10            }
+11            else {
+12                res++;
+13            }
+14        }
+15
+16        return res;
+17    }
+18};
