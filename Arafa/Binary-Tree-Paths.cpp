7e6efb4d2a9dc279b6ce8b5102f9dd7ddc81f3d2
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-17 22:40:53 +0200

    [257] [Time Beats: 100.00%] [Memory Beats: 5.51%] - LeetPush

--- Arafa/Binary-Tree-Paths.cpp
+++ Arafa/Binary-Tree-Paths.cpp
@@ -1,39 +1,47 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<string> answer;
-    void solve(TreeNode* root, vector<int> result) {
-        if(root == nullptr) return;
-
-        if(root->left == nullptr && root->right == nullptr) {
-            string temp = "";
-        
-            for (size_t i = 0; i < result.size(); ++i) {
-                
-                temp += std::to_string(result[i]);
-                temp += "->";
-            }
-            temp += std::to_string(root->val);
-            answer.push_back(temp);
-        }
-        result.push_back(root->val);
-
-        solve(root->left, result);
-        solve(root->right, result);
-    }
-    vector<string> binaryTreePaths(TreeNode* root) {
-        vector<int> result;
-        solve(root, result);
-        return answer;
-    }
-};
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    vector<string> ans;
+15
+16    void solve(TreeNode* root, vector<int> vec){
+17        if(root == nullptr) 
+18            return;
+19        
+20        if(root->left == nullptr && root->right == nullptr) {
+21            string temp = "";
+22            for(int i = 0; i < vec.size(); i++) {
+23                if(i == 0)
+24                    temp = to_string(vec[i]);
+25                else
+26                    temp += "->" + to_string(vec[i]);
+27            }
+28            if(temp == "")
+29                temp += to_string(root->val);
+30            else 
+31                temp += "->" + to_string(root->val);
+32            ans.push_back(temp);
+33            return;
+34        }
+35
+36        vec.push_back(root->val);
+37        solve(root->left, vec);
+38        solve(root->right, vec);
+39        vec.pop_back();
+40    }
+41
+42    vector<string> binaryTreePaths(TreeNode* root) {
+43        solve(root, {});
+44
+45        return ans;
+46    }
+47};
