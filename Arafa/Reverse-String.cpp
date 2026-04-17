341df73511dda95ab40ed890049eee3a8fa5d501
AhmmedArafa <ahmad.arafa@cairomotive.com>
2026-04-17 21:50:48 +0200

    [344] [Time Beats: 100.00%] [Memory Beats: 51.33%] - LeetPush

--- Arafa/Reverse-String.cpp
+++ Arafa/Reverse-String.cpp
@@ -1,14 +1,15 @@
-class Solution {
-public:
-    void reverseString(vector<char>& s) {
-        int left = 0;
-        int right = s.size()-1;
-        while(left < right) {
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
-        }
-    }
-};
+1class Solution {
+2public:
+3    void reverseString(vector<char>& s) {
+4        int left = 0;
+5        int right = s.size() - 1;
+6
+7        while(left < right) {
+8            char temp = s[left];
+9            s[left] = s[right];
+10            s[right] = temp;
+11            left++;
+12            right--;
+13        }
+14    }
+15};
